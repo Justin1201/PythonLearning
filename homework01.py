148a0514af29d22fe84bd25d827acedabe616793
Justin1201 <40173801+Justin1201@users.noreply.github.com>
2018-06-24 17:09:27 +0800

    Add files via upload

---
+++ homework01.py
@@ -0,0 +1,13 @@
+n1=input("Enter a Number:")
+n2=input("Enter a Number:")
+n3=input("Enter a Number:")
+n4=input("Enter a Number:")
+n5=input("Enter a Number:")
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+result=n1+n2+n3+n4+n5
+print(result)
+print(max(n1,n2,n3,n4,n5))
